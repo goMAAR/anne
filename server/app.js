e4b553d80befcab6d3bdcd2a4effa37af94165f0
ladyshaitan <hello.anne.pruett@gmail.com>
2017-12-18 11:53:25 -0800

    Write out request paths

--- server/app.js
+++ server/app.js
@@ -1,18 +1,36 @@
+/*eslint-disable*/
 const express = require('express');
-const path = require ('path');
+// const path = require('path');
 const app = express();
 const http = require('http').Server(app);
 const bodyParser = require('body-parser');
+const { createTweet } = require('../database/dbHelpers.js');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.get('/', (req, res) => {
-  console.log('hey im workin');
-  res.send(200);
+  res.sendStatus(200);
 });
 
-http.listen(3000, ()=>{
+app.get('/feed', (req, res) => {
+	res.sendStatus(200);
+});
+
+app.post('/metrics', (req, res) => {
+	res.sendStatus(201);
+});
+
+app.post('/tweet/create', createTweet, (req, res) => {
+	res.sendStatus(201);
+});
+
+app.post('/user/create', (req, res) => {
+	res.sendStatus(201);
+});
+
+http.listen(3000, () => {
   console.log('listening on 3000');
 });
 
