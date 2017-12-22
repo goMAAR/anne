573869d3da7dd50e72b9977ab9200ac3d318589e
ladyshaitan <hello.anne.pruett@gmail.com>
2017-12-21 16:46:09 -0800

    Build helpers to insert tweets and follows into lucene, then delete job from queue

--- database/dbHelpers.js
+++ database/dbHelpers.js
@@ -1,11 +1,84 @@
 /* eslint-disable */
 const { client } = require('./db.js');
 
-const createTweet = (req, res, next) => {
-  // client.create code here
-  next();
+const insertTweet = (tweet, done) => {
+  let { id, user_id, created_at } = tweet;
+  let record = {
+    index: 'slimtweets',
+    type: 'tweet',
+    id: id,
+    body: {
+      user_id: user_id,
+      created_at: created_at
+    }
+  };
+
+	client.create(record, (err, resp) => {
+    if(err) { console.log(err); }
+    else {
+      console.log('record inserted successfully, record:', tweet);
+      done();
+    }
+  });
 }
 
-module.exports.createTweet = createTweet;
+const insertFollow = (follow, done) => {
+  console.log('hitting, follow: ', follow);
+  let { id, follower_id, followee_id, created_at } = follow;
+  let record = {
+    index: 'follows',
+    type: 'follow',
+    id: id,
+    body: {
+      follower_id: follower_id,
+      followee_id: followee_id,
+      created_at: created_at
+    }
+  };
+
+  client.create(record, (err, resp) => {
+    if(err) { console.log(err); }
+    else {
+      console.log('record inserted successfully, record:', resp);
+      done();
+    }
+  })
+}
+
+// needs reworking
+const insertALotOfTweets = (tweetArray) => {
+  let { id, user_id, created_at } = req.body;
+  let arrayOfTweets = [];
+
+  let tweetMetaData = {
+    index: {
+      _index: 'slimtweets',
+      type: 'tweet',
+      _id: id
+    }
+  };
+
+  let tweet = {
+    id: id,
+    user_id: user_id,
+    created_at: created_at
+  }
+
+  arrayOfTweets.push(tweetMetaData);
+  arrayOfTweets.push(tweet);
+
+  client.bulk({
+    body: arrayOfTweets
+  }, (err, resp) => {
+    if(err) { console.log(err); }
+    else {
+      console.log('bulk load')
+    }
+  }); 
+}
+
+module.exports.insertTweet = insertTweet;
+module.exports.insertALotOfTweets = insertALotOfTweets;
+module.exports.insertFollow = insertFollow;
 
 // helpers for creating documents and querying for data
